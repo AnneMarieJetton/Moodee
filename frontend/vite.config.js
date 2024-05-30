d1a7f83524b4e0eaed65949e79775e7476c31d81
Jenna Cioffi <JMCioffi4@gmail.com>
2024-05-30 12:05:45 -0600

    Updated for hosting ability on other local devices

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows access from any IP address
+    port: 4444, // You can use any port, but ensure it's not in use
+  }
 })
