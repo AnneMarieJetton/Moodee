81ed1ba5b3a34beea6ca7b60eacddf9d02bad182
Jenna Cioffi <JMCioffi4@gmail.com>
2024-05-30 12:36:59 -0600

    Created Progress Bar Component
    This progress bar is just a skeleton.
    
    TODO: create individual question components and link with progress bar

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
+import ProgressBar from '../components/Quiz/ProgressBar';
+import '../styles/Quiz.css';
+
 const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    return (
-
-        <div className = 'Stats'>
-            Quiz page
-        </div>
-    );
+  return (
+    <div className='Quiz'>
+      <ProgressBar currentQuestion={currentQuestion} />
+      {/* Outer Question Component (that takes in a question component) */}
+      {/* Skip Button */}
+    </div>
+  );
 };
 
-export default Quiz
+export default Quiz;
